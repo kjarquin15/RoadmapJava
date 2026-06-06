11e7a72afb0ccb2e4ecf26de3f7b109e9395c41f
KAREN LIZZETH JARQUÍN SALGUERO <kjarquins@miumg.edu.gt>
2026-06-05 19:58:25 -0600

    feat: classesobjects

---
+++ src/classesobjects/Person.java
@@ -0,0 +1,11 @@
+package classesobjects;
+
+public class Person {
+	// Attribute
+    String name;
+
+    // Method
+    public void greet() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
