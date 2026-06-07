5095d77b41f875b3ded521151f1ccd7699d6a6ee
KAREN LIZZETH JARQUÍN SALGUERO <kjarquins@miumg.edu.gt>
2026-06-06 21:53:58 -0600

    feat: Type Casting

---
+++ src/typecasting/TypeCastingDemo.java
@@ -0,0 +1,13 @@
+package typecasting;
+
+public class TypeCastingDemo {
+	 public void mostrar() {
+
+	        double numeroDecimal = 9.8;
+
+	        // Conversión explícita
+	        int numeroEntero = (int) numeroDecimal;
+
+	        System.out.println(numeroEntero);
+	    }
+}
